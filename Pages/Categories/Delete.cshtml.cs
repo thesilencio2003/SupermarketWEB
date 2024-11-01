7883957366e21ee20b1a722cf0dbec72e61a757c
Alejandro <alejohola24@gmail.com>
2024-11-01 00:37:21 -0500

    Delete  Categories

---
+++ Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SumpermarketContext _context;
+
+        public DeleteModel(SumpermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Category Category { get; set; } = default!;
+
+        public async Task<IActionResult>OnGetAsync(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Category = category;
+            }
+
+            return Page();
+
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories.FindAsync(id);
+
+
+            if (category != null)
+            {
+                Category = category;
+                _context.Categories.Remove(Category);
+                await _context.SaveChangesAsync();
+
+            }   
+
+            return RedirectToPage("./Index");
+        }
+
+    }
+}
