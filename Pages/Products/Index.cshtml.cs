91bd037ecef94496c510c6b9412c4c2e426c5242
Alejandro <alejohola24@gmail.com>
2024-11-01 12:24:37 -0500

    index products

---
+++ Pages/Products/Index.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Products
+{
+    public class IndexModel : PageModel
+    {
+
+        private readonly SumpermarketContext _context;
+
+        public IndexModel(SumpermarketContext context)
+        {
+            _context = context;
+        }
+     
+        public IList<Product> Products { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Products != null)
+            {
+                Products = await _context.Products.ToListAsync();
+
+            }
+        }
+
+    }
+}
