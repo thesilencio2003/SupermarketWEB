94f2c064172fc3fc178163d6796eedeb36e5e0a5
Alejandro <alejohola24@gmail.com>
2024-11-01 00:17:24 -0500

    Create  Categories

---
+++ Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SumpermarketContext _context;
+
+        public CreateModel(SumpermarketContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public
+         Category Category
+        { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Categories == null || Category == null)
+            {
+                return Page();
+
+            }
+
+            _context.Categories.Add(Category);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+
+        }
+    }
+}
