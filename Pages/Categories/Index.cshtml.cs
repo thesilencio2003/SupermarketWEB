cd5a96577485f1ae22eb4208d5471265893349fa
Alejandro <alejohola24@gmail.com>
2024-10-31 23:37:42 -0500

    Index Categories

---
+++ Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SumpermarketContext _context;
+
+        public IndexModel(SumpermarketContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Categories { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Categories != null)
+            {
+                Categories = await _context.Categories.ToListAsync();
+
+            }
+        }
+    }
+}
