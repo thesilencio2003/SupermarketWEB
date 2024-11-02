6d7a5de20aa3a97249127aca133af2185800b91f
Alejandro <alejohola24@gmail.com>
2024-11-01 22:12:38 -0500

    Edit  Products

---
+++ Pages/Products/Edit.cshtml.cs
@@ -0,0 +1,76 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Products
+{
+    public class EditModel : PageModel
+    {
+        private readonly SumpermarketContext _context;
+
+        public EditModel(SumpermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Product Product { get; set; } = default!;
+
+        public async
+         Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            Product = product;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+
+            {
+                if (!CategoryExists(Product.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return (_context.Categories?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+    }
+}
+
