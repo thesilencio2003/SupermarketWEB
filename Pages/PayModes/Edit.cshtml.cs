22dc8ec02bba4d66459db13826ad91675e42b29b
Alejandro <alejohola24@gmail.com>
2024-11-02 00:36:40 -0500

    Edit PayMode

---
+++ Pages/PayModes/Edit.cshtml.cs
@@ -0,0 +1,75 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.PayModes
+{
+    public class EditModel : PageModel
+    {
+        private readonly SumpermarketContext _context;
+
+        public EditModel(SumpermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public PayMode PayMode { get; set; } = default!;
+
+        public async
+         Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.PayModes == null)
+            {
+                return NotFound();
+            }
+
+            var payMode = await _context.PayModes.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (payMode == null)
+            {
+                return NotFound();
+            }
+
+            PayMode = payMode;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(PayMode).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+
+            {
+                if (!CategoryExists(PayMode.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return (_context.PayModes?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+    }
+}
