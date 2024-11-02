8085d0b6fa063fa3822581b480c24124268c59c4
Alejandro <alejohola24@gmail.com>
2024-11-01 22:53:02 -0500

    Delete Providers

---
+++ Pages/Providers/Delete.cshtml.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Providers
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SumpermarketContext _context;
+
+        public DeleteModel(SumpermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Provider Provider { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Providers == null)
+            {
+                return NotFound();
+            }
+
+            var provider = await _context.Providers.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (provider == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Provider = provider;
+            }
+
+            return Page();
+
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Providers == null)
+            {
+                return NotFound();
+            }
+
+            var provider = await _context.Providers.FindAsync(id);
+
+
+            if (provider != null)
+            {
+                Provider = provider;
+                _context.Providers.Remove(Provider);
+                await _context.SaveChangesAsync();
+
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+    }
+}
