03a52b267b049ecfbfeb963329f6c911a5905045
Alejandro <alejohola24@gmail.com>
2024-11-02 00:29:28 -0500

    Delete PayMode

---
+++ Pages/PayModes/Delete.cshtml.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.PayModes
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SumpermarketContext _context;
+
+        public DeleteModel(SumpermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public PayMode PayMode { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.PayModes == null)
+            {
+                return NotFound();
+            }
+
+            var payMode = await _context.PayModes.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (payMode == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                PayMode = payMode;
+            }
+
+            return Page();
+
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.PayModes == null)
+            {
+                return NotFound();
+            }
+
+            var payMode = await _context.PayModes.FindAsync(id);
+
+
+            if (payMode != null)
+            {
+                PayMode = payMode;
+                _context.PayModes.Remove(PayMode);
+                await _context.SaveChangesAsync();
+
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+    }
+}
