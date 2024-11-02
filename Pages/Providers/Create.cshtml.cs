79442a66a29ffa7e01e389db7a4674bd0a4d15a7
Alejandro <alejohola24@gmail.com>
2024-11-01 22:42:20 -0500

    Create Providers

---
+++ Pages/Providers/Create.cshtml.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Providers
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SumpermarketContext _context;
+
+        public CreateModel(SumpermarketContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public
+        Provider Provider
+        { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Providers == null || Provider == null)
+            {
+                return Page();
+
+            }
+
+            _context.Providers.Add(Provider);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+
+        }
+
+    }
+}
